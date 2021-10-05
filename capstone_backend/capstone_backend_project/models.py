0d9af8d63fec8ccd7c9388ce984a6a57efc610d3
armoredpigeon1111 <armoredpigeon1111@outlook.com>
2021-10-05 16:55:45 -0400

    Updated models to have autocreated Id's

--- capstone_backend/capstone_backend_project/models.py
+++ capstone_backend/capstone_backend_project/models.py
@@ -2,14 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Musician(models.Model):
-    musician_id = models.IntegerField()
     username = models.CharField(max_length=50)
     bandname = models.CharField(max_length=50)
     genre = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
 
 class Gig(models.Model):
-    gig_id = models.IntegerField()
     musician_id = models.IntegerField(null=False)
     street = models.CharField(max_length=50)
     city = models.CharField(max_length=20)
@@ -19,7 +17,6 @@ class Gig(models.Model):
     rsvps = models.IntegerField(max_length=10)
 
 class Review(models.Model):
-    review_id = models.IntegerField()
     gig_id = models.IntegerField(null=False)
     body = models.CharField(max_length=500)
 
